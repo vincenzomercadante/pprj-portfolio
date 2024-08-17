ffa431f048bbf66dc0493b2246cc855046454a3e
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-08-17 10:33:19 +0200

    fix: hire section height

--- src/components/sections/AppHire.vue
+++ src/components/sections/AppHire.vue
@@ -5,69 +5,57 @@ export default {};
 <template>
   <section
     id="hire"
-    class="page-section d-flex justify-content-center align-items-center flex-column"
+    class="page-section d-flex align-items-center flex-column pb-3"
   >
     <!-- section's title -->
     <h2 class="section-title fw-semibold">hire me</h2>
     <div class="container">
       <div class="row g-4">
         <!-- contact detail section -->
-        <div class="col-6 contact-detail d-flex flex-column align-items-center">
+        <div class="col-7 contact-detail d-flex flex-column">
           <!-- section's title -->
-          <h3 class="mt-5 mb-3 text-center">Contact Details:</h3>
+          <h3 class="mt-4 mb-3">Contact Details:</h3>
           <!-- mail detail -->
-          <div
-            class="mt-5 d-flex align-items-center justify-content-center gap-2"
-          >
+          <div class="mt-4 d-flex align-items-center gap-2">
             <font-awesome-icon icon="fa-solid fa-envelope" />
-            <h4 class="m-0 text-center">Mail:</h4>
+            <h4 class="m-0">Mail:</h4>
           </div>
 
           <!-- mail -->
-          <a
-            href="mailto:vincenzo.mercadante0@outlook.it"
-            class="text-center fs-5"
+          <a href="mailto:vincenzo.mercadante0@outlook.it" class="fs-5"
             >vincenzo.mercadante0@outlook.it</a
           >
 
           <!-- cellular number detail -->
-          <div
-            class="mt-5 d-flex align-items-center justify-content-center gap-2"
-          >
+          <div class="mt-4 d-flex align-items-center gap-2">
             <font-awesome-icon icon="fa-solid fa-phone" />
-            <h4 class="m-0 text-center">Cellular Number:</h4>
+            <h4 class="m-0">Cellular Number:</h4>
           </div>
 
           <!-- cellular -->
           <p class="m-0 fs-5">+39 3458430577</p>
 
           <!-- linkedin detail -->
-          <div
-            class="mt-5 d-flex align-items-center justify-content-center gap-2"
-          >
+          <div class="mt-4 d-flex align-items-center gap-2">
             <font-awesome-icon icon="fa-brands fa-linkedin" />
-            <h4 class="m-0 text-center">Linkedin:</h4>
+            <h4 class="m-0">Linkedin:</h4>
           </div>
 
           <!-- linkedin link -->
           <a
             href="https://www.linkedin.com/in/vincenzo-mercadante-262357211/"
-            class="text-center fs-5"
+            class="fs-5"
             >https://www.linkedin.com/in/vincenzo-mercadante-262357211/</a
           >
 
           <!-- github detail -->
-          <div
-            class="mt-5 d-flex align-items-center justify-content-center gap-2"
-          >
+          <div class="mt-4 d-flex align-items-center gap-2">
             <font-awesome-icon icon="fa-brands fa-github" />
-            <h4 class="m-0 text-center">Github:</h4>
+            <h4 class="m-0">Github:</h4>
           </div>
 
           <!-- github link -->
-          <a
-            href="https://github.com/vincenzomercadante"
-            class="text-center fs-5"
+          <a href="https://github.com/vincenzomercadante" class="fs-5"
             >https://github.com/vincenzomercadante</a
           >
 
@@ -75,7 +63,7 @@ export default {};
           <a
             href="../../../src/assets/file-to-download/Vincenzo Mercadante_CV.pdf"
             download="Vincenzo-Mercadante_CV.pdf"
-            class="btn-link"
+            class="btn-link align-self-center mt-5"
           >
             <font-awesome-icon icon="fa-solid fa-download" />
             Download CV!
@@ -83,12 +71,12 @@ export default {};
         </div>
 
         <!-- cv download -->
-        <div class="col-6 cv-download">
+        <div class="col-5 cv-download">
           <!-- cv screenshot with file download as href -->
           <a
             href="../../../src/assets/file-to-download/Vincenzo Mercadante_CV.pdf"
             download="Vincenzo-Mercadante_CV.pdf"
-            class="position-relative d-flex w-100 h-100 justify-content-center cv-download"
+            class="position-relative d-flex cv-download"
             ><img
               src="../../../src/assets/file-to-download/cv.png"
               alt="cv image"
@@ -118,10 +106,6 @@ export default {};
   color: $main-color;
 }
 
-.btn-link {
-  margin-top: 100px;
-}
-
 .cv-download {
   .layover {
     flex-direction: column;
@@ -134,7 +118,10 @@ export default {};
   }
 
   img {
-    height: 800px;
+    width: 100%;
+    height: 650px;
+    object-fit: cover;
+    object-position: top;
   }
 
   &:hover .layover {
