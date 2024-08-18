8a6b22ed416c47ffc2097298c191426c97572ff7
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-08-18 12:21:40 +0200

    feat: add project card placeholder

--- src/components/sections/AppProjects.vue
+++ src/components/sections/AppProjects.vue
@@ -5,12 +5,87 @@ export default {};
 <template>
   <section id="projects" class="page-section">
     <h2 class="section-title fw-semibold">my projects</h2>
+
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-4">
+          <section class="projects-section">
+            <div class="video-carousel">
+              <div class="video-player-placeholder">
+                <font-awesome-icon icon="fa-solid fa-play" />
+              </div>
+            </div>
+            <div class="paragraph-carousel p-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio voluptates ea sint, reprehenderit doloribus enim itaque
+              perspiciatis porro consequatur numquam beatae, cupiditate placeat
+              deleniti. Provident nam et nesciunt iure commodi?
+            </div>
+          </section>
+        </div>
+        <div class="col-4">
+          <section class="projects-section">
+            <div class="video-carousel">
+              <div class="video-player-placeholder">
+                <font-awesome-icon icon="fa-solid fa-play" />
+              </div>
+            </div>
+            <div class="paragraph-carousel p-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio voluptates ea sint, reprehenderit doloribus enim itaque
+              perspiciatis porro consequatur numquam beatae, cupiditate placeat
+              deleniti. Provident nam et nesciunt iure commodi?
+            </div>
+          </section>
+        </div>
+        <div class="col-4">
+          <section class="projects-section">
+            <div class="video-carousel">
+              <div class="video-player-placeholder">
+                <font-awesome-icon icon="fa-solid fa-play" />
+              </div>
+            </div>
+            <div class="paragraph-carousel p-3">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio voluptates ea sint, reprehenderit doloribus enim itaque
+              perspiciatis porro consequatur numquam beatae, cupiditate placeat
+              deleniti. Provident nam et nesciunt iure commodi?
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
+@use "../../assets/styles/partials/mixins" as *;
+
 #projects {
-  height: 100vh;
-  color: white;
+  .projects-section {
+    height: 500px;
+    border: 2px solid white;
+
+    .video-carousel {
+      color: white;
+      @include center-div;
+      height: 60%;
+      background-color: olivedrab;
+
+      .video-player-placeholder {
+        padding: 16px;
+        @include container-rounded;
+        @include center-div;
+        border: 3px solid white;
+        font-size: 1.5rem;
+        cursor: pointer;
+      }
+    }
+
+    .paragraph-carousel {
+      height: 40%;
+      background-color: white;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -21,6 +21,7 @@ import {
   faDownload,
   faPhone,
   faEnvelope,
+  faPlay,
 } from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
@@ -32,7 +33,8 @@ library.add(
   faLock,
   faDownload,
   faPhone,
-  faEnvelope
+  faEnvelope,
+  faPlay
 );
 
 // create app
